93467e5f5b2a04af719378fba3b984109d01335c
hamedakbarii <hamedkarate0@gmail.com>
2022-07-03 14:07:55 +0430

    service section created

--- script/script.js
+++ script/script.js
@@ -23,27 +23,3 @@ deletBtn.addEventListener('click', () => {
 
 
 
-// --------------------- Swiper ---------------------------//
-// import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
-
-
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
