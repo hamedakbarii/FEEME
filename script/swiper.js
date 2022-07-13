ab4e9629482d1e7cc40500dffb3a2ee43901f9dc
hamedakbarii <hamedkarate0@gmail.com>
2022-07-13 17:43:57 +0430

    swiper added on hero section

--- script/swiper.js
+++ script/swiper.js
@@ -1,10 +1,6 @@
-// --------------------- Swiper ---------------------------//
-import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
-
-
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  loop: true,
+ 
 
   // If we need pagination
   pagination: {
